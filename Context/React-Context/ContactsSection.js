7cda8a07fa3cfe02fa2f2a3676ba093c31881a4f
unknown <joshua.a.disney@gmail.com>
2023-03-19 23:42:37 -0700

    Lesson 6/7 for context

--- Context/React-Context/ContactsSection.js
+++ Context/React-Context/ContactsSection.js
@@ -1,10 +1,26 @@
+// import React from "react";
+// import { ContactsList } from "./ContactsList";
+
+// export const ContactsSection = ({ contacts, name }) => {
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+//       <ContactsList contacts={contacts} />
+//     </div>
+//   );
+// };
+
+
+
 import React from "react";
 import { ContactsList } from "./ContactsList";
+import { ThemeSwitcher } from "./ThemeSwitcher";
 
 export const ContactsSection = ({ contacts, name }) => {
   return (
     <div>
       <h2>{name}</h2>
+      <ThemeSwitcher />
       <ContactsList contacts={contacts} />
     </div>
   );
--- Context/React-Context/ThemeContext.js
+++ Context/React-Context/ThemeContext.js
@@ -1,4 +1,14 @@
-// Put your code here! ✨
-import React from 'react'
+import React, { useState } from "react";
 
-export const ThemeContext = React.createContext()
+export const ThemeContext = React.createContext();
+
+export const ThemeArea = ({ children, initialTheme }) => {
+  // Put your code here! ✨
+  const [theme, setTheme] = useState(initialTheme)
+  
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
